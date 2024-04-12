b10ad3025c9189a723b690b9d98d9a6a1c732e4d
Antonio Lindo <antoniolvitall@gmail.com>
2024-04-12 07:46:47 -0300

    req 1 3/4

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import { createUser } from '../../services/userAPI';
+import { UserType } from '../../types';
 
 function Login() {
+  const user = {};
+  const [userName, setUserName] = React.useState<string>('');
+
+  function isFormValid() {
+    return userName.length >= 3;
+  }
   return (
     <form action="">
       <label>
         Login
-        <input type="text" placeholder="Username" data-testid="login-name-input" />
+        <input
+          type="text"
+          placeholder="Username"
+          data-testid="login-name-input"
+          value={ userName }
+          onChange={ ({ target }) => setUserName(target.value) }
+        />
       </label>
       <button
         type="submit"
         data-testid="login-submit-button"
-        onSubmit={ createUser }>
+        onClick={ () => createUser(user as UserType) }
+        disabled={ !isFormValid() }
+      >
         Entrar
       </button>
     </form>
